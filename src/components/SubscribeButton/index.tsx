3e35fe755defe8e78e20f8014094ed907d614653
Roger Fernandes <roger.afernandes@hotmail.com>
2021-03-24 09:54:13 -0300

    evitando duplicação no stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { signIn, useSession } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -8,19 +10,33 @@ interface SubscribeButtonProps {
 export function SubscribeButton({priceId} : SubscribeButtonProps) {
   const [session] = useSession();
   
-  function handleSubscribe(){
+  async function handleSubscribe(){
     if(!session){
       signIn('github')
       return;
     }
 
     //criação da checkout session
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      //para reridicionar
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({sessionId});
+    } catch (error) {
+      alert(error.message);
+    }
+
   }
 
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
