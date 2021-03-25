e6eb4182868b180525d568ac5b057de3e4ca86ce
Roger Fernandes <roger.afernandes@hotmail.com>
2021-03-25 15:36:57 -0300

    Validando assinatura ativa

---
+++ src/pages/posts/preview/[slug].tsx
@@ -0,0 +1,70 @@
+import { GetStaticProps } from "next"
+import { getSession } from "next-auth/client"
+import Head from "next/head";
+import { RichText } from "prismic-dom";
+import React from "react";
+import { getPrismicClient } from "../../../services/prismic";
+
+import styles from '../post.module.scss';
+
+type PostPreviewProps = {
+  post: {
+    slug: string;
+    title: string;
+    content: string;
+    updatedAt: string;
+  }
+}
+
+export default function Post({ post }: PostPreviewProps){
+  return(
+    <>
+      <Head>
+        <title>{post.title} | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <article className={styles.post}>
+          <h1>{post.title}</h1>
+          <time>{post.updatedAt}</time>
+          {/* colocar html pronto */}
+          <div 
+            className={`${styles.postContent} ${styles.previewContent}`}
+            dangerouslySetInnerHTML={{__html: post.content}}
+          />
+        </article>
+      </main>
+    </>
+  )
+}
+
+export const getStaticPaths = () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params;
+
+  const prismic = getPrismicClient();
+
+  const response = await prismic.getByUID('post', String(slug), {})
+
+  const post = {
+    slug,
+    title: RichText.asText(response.data.title),
+    content: RichText.asHtml(response.data.content.splice(0, 3)),
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric',
+    })
+  };
+
+  return({ 
+    props: { post }
+  })
+
+}
